8e2f7b64a0c2963390112e674738da01c79d4b80
Alex Southgate <southgateJA@cardiff.ac.uk>
2025-07-19 10:56:17 +0100

    Remove some junk comments

--- tests/test_compute.cpp
+++ tests/test_compute.cpp
@@ -51,30 +51,3 @@ TEST_CASE("Compute pipeline processes data for double", "[vulkan]") {
     }
     SUCCEED("Compute shader produced expected results for doubles * 2.");
 }
-
-
-// TEST_CASE("Compute pipeline correctly runs for 1 shader/1 layout/2 bindings", "[vulkan]") {
-//     mynydd::VulkanContext context = mynydd::createVulkanContext();
-//     std::shared_ptr<mynydd::VulkanContext> contextPtr = std::make_shared<mynydd::VulkanContext>(context);
-//     mynydd::ComputeEngine<double> pipeline(contextPtr, "shaders/shader_double_2bindings.comp.spv");
-
-//     std::vector<double> inputData1(1024);
-//     std::vector<double> inputData2(1024);
-//     for (size_t i = 0; i < inputData1.size(); ++i) {
-//         inputData1[i] = static_cast<double>(i);
-//         inputData1[2] = 2.0 * static_cast<double>(i);
-//     }
-
-//     pipeline.createDynamicResources(1024);
-    
-//     pipeline.uploadData(inputData1, 0);
-//     pipeline.uploadData(inputData2, 1);
-
-//     std::vector<double> output = pipeline.execute();
-//     for (size_t i = 0; i < std::min<size_t>(output.size(), 10); ++i) {
-//         std::cout << "output[" << i << "] = " << output[i] << std::endl;
-//         REQUIRE(output[i] == static_cast<double>(i) * 2.0);
-//     }
-//     SUCCEED("Compute shader produced expected results for doubles * 2.");
-// }
-
