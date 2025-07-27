d35db98f047a8679419beae89c557923d135d748
Alex Southgate <southgateJA@cardiff.ac.uk>
2025-07-27 16:04:31 +0100

    Add some sph example kernels

--- examples/sph/main.cpp
+++ examples/sph/main.cpp
@@ -3,4 +3,24 @@
 
 int main(int argc, char** argv) {
     std::cout << "Running SPH example..." << std::endl;
+
+    mynydd::VulkanContext context = mynydd::createVulkanContext();
+    std::shared_ptr<mynydd::VulkanContext> contextPtr = std::make_shared<mynydd::VulkanContext>(context);
+    mynydd::VulkanDynamicResources dynamicResources = mynydd::createDataResources<float>(contextPtr, 1024);
+    std::shared_ptr<mynydd::VulkanDynamicResources> dynamicResourcesPtr = std::make_shared<mynydd::VulkanDynamicResources>(dynamicResources);
+    mynydd::ComputeEngine<float> compeng(contextPtr, dynamicResourcesPtr, "shader.comp.spv");
+
+    std::vector<float> inputData(1024);
+    for (size_t i = 0; i < inputData.size(); ++i) {
+        inputData[i] = static_cast<float>(i);
+    }
+
+    compeng.uploadData(inputData);
+
+
+    std::vector<float> output = compeng.execute();
+    for (size_t i = 1; i < std::min<size_t>(output.size(), 10); ++i) {
+        std::cout << "output[" << i << "] = " << output[i] << std::endl;
+    }
+
 }
