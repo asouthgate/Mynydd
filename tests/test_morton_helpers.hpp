5be85805c55599ad162f5c9c6f6592a17d4cc109
Alex Southgate <southgateJA@cardiff.ac.uk>
2025-08-16 13:28:18 +0100

    Get tests working and compiling after extracting morton helpers

---
+++ tests/test_morton_helpers.hpp
@@ -0,0 +1,26 @@
+#ifndef TEST_MORTON_HELPERS_HPP
+#define TEST_MORTON_HELPERS_HPP
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+#include <glm/glm.hpp>
+#include <memory>
+#include <vector>
+#include <mynydd/mynydd.hpp>
+
+
+struct Particle {
+    glm::vec3 position;
+    uint32_t key;
+};
+
+struct KeyRange {
+    uint32_t keyMin;
+    uint32_t keyMax;
+};
+
+
+std::vector<KeyRange> computeKeyRanges(const std::vector<Particle>& particles, float dmax);
+std::vector<uint32_t> runMortonTest(std::shared_ptr<mynydd::VulkanContext> contextPtr, const uint32_t nBits);
+
+#endif // TEST_MORTON_HELPERS_HPP
